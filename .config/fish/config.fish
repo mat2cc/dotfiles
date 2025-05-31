cd3071f2c3af07cc11ec4def8df91e68a96a570a
Matt Christofides <matthewchristofides@gmail.com>
2025-05-31 12:23:48 -0400

    cleanup fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,20 @@
-alias ls exa
-alias l "exa -lah --icons"
+pyenv init - fish | source
+
+alias ls eza
+alias l "eza -lah --icons"
 alias vi nvim
 alias vim nvim
-
 
 alias gs "git status"
 alias gl "git log"
 
-# eval (ssh-agent -c)
-# ssh-add -l > /dev/null || ssh-add
+alias git-recent "git branch --sort=-committerdate"
+
+alias flush_dns "sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+
+alias aws-us-new-account "aws-vault exec prod-us --"
+alias aws-us "aws-vault exec prod-us-old --"
+alias aws-us-admin "aws-vault exec prod-us-admin --"
+alias aws-new-staging "aws-vault exec new-staging --"
+
+alias docker-login "aws ecr get-login-password | docker login --username AWS --password-stdin 189785257069.dkr.ecr.us-east-1.amazonaws.com"
